b1f5bfdaab0189e1a9d7ef6e83c0a82b4dce195c
han <chos9715@naver.com>
2022-07-18 21:45:02 +0900

    6 재귀함수

---
+++ src/RecursivePractice.java
@@ -0,0 +1,17 @@
+public class RecursivePractice {
+    static int factorial(int n){
+        if (n == 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args){
+        System.out.println(factorial(1));
+        System.out.println(factorial(2));
+        System.out.println(factorial(3));
+        System.out.println(factorial(4));
+        System.out.println(factorial(5));
+    }
+
+}
